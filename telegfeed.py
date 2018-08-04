395a5046ec6adf2a507163db9368c8f047979a76
Mohammad Zolfaghari <zidom72@gmail.com>
2018-08-04 13:41:36 +0430

    simple generating feed from channel

--- telegfeed.py
+++ telegfeed.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify
-from telegram import generate_channel_feed
+from telegram import get_posts
+from werkzeug.contrib.atom import AtomFeed
 
-app = Flask(__name__, static_url_path='./')
+app = Flask(__name__)
 
 @app.route('/channel/<channel_id>')
 def channel_feed(channel_id):
-    return jsonify(generate_channel_feed(channel_id))
+    feed = AtomFeed('۱۰ نوشته آخر کانال فلان', id='1')
+
+    posts = get_posts(channel_id)
+    for post in posts:
+        if not post.edit_date:
+            post.edit_date = post.date
+
+        feed.add('test', content=post.message, id=post.id,
+            updated=post.edit_date, published=post.date)
+
+    return feed.get_response()
--- telegram.py
+++ telegram.py
@@ -1,15 +1,14 @@
-from telethon import TelegramClient, sync
-import json
-import config, utils
+from telethon import TelegramClient
+import config
 
 client = TelegramClient('telegfeed',
 	config.TELEGRAM['app_id'], config.TELEGRAM['api_hash']).start()
 
-def generate_channel_feed(channel_id, limit=10):
+def get_posts(channel_id, limit=10):
     channel = client.get_input_entity(channel_id)
 
-    msgs = []
-    for msg in client.iter_messages(channel, limit=10):
-        msgs.append(msg.to_dict())
+    posts = []
+    for post in client.iter_messages(channel, limit=limit):
+        posts.append(post)
 
-    return msgs
+    return posts
