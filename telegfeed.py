7155e134bf5b6b960310900738d015a9d6bc1e07
Mohammad Zolfaghari <zidom72@gmail.com>
2018-08-04 13:05:35 +0430

    preparing a frontend for later use

--- telegfeed.py
+++ telegfeed.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from telegram import generate_channel_feed
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='./')
 
 @app.route('/channel/<channel_id>')
 def channel_feed(channel_id):
