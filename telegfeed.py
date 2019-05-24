04b0983d12aedc4a8b3371fd01968ac8b373d8d7
Mohammad Zolfaghari <zidom72@gmail.com>
2019-05-25 00:29:04 +0430

    show channel posts in html

--- telegfeed.py
+++ telegfeed.py
@@ -9,6 +9,14 @@ def index():
     return render_template('index.html')
 
 @app.route('/<name>')
+def channel(name):
+    channel = Channel(name)
+
+    posts = channel.get_posts()
+
+    return render_template('channel.html', posts=posts)
+
+@app.route('/<name>/feed')
 def channel_feed(name):
     channel = Channel(name)
 
